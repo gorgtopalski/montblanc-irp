d3e3d56b21365a4fcf31d93c29222fc8718e16a9
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-06 12:50:51 +0200

    feat: entities Views

---
+++ Vanilla-IRP/View/ListDefectView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Vanilla_IRP.ViewModel;
+
+namespace Vanilla_IRP.View
+{
+    public partial class ListDefectView : UserControl
+    {
+        public ListDefectView()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowSelected(object sender, MouseButtonEventArgs e)
+        {
+            if (this.DataContext is ListDefectViewModel vm)
+                vm.EditSelectedCommand.Execute(null);
+        }
+    }
+}
