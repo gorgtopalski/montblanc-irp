fa0f9073607962460a9d0d5fb66fa5d425f0007b
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-06 12:48:38 +0200

    feat: specific db service to query "defect" table

---
+++ Vanilla-IRP/Services/DefectService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Vanilla_IRP.Domain;
+
+namespace Vanilla_IRP.Services
+{
+    public class DefectService : BaseService<Defect>
+    {
+        public DefectService() : base() {}
+
+        public override List<Defect> GetAll() => Db.Query<Defect>().Include(x => x.DefectType).ToList();
+    }
+}
