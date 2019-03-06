d3becbf0224a80c4ec434eda6cb9f7dd5ffcf8f3
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-06 12:47:50 +0200

    feat: generic service for db query based on the domain entity

---
+++ Vanilla-IRP/Services/IDomainService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanilla_IRP.Domain;
+
+namespace Vanilla_IRP.Services
+{
+    interface IDomainService<T> where T : IDomain
+    {
+        void Save(T item);
+
+        void Delete(T item);
+
+        List<T> GetAll();
+    }
+}
