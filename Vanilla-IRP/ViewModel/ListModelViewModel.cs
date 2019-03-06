ac1b519088d5a539cd24201259f48b4deebcebda
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-06 12:50:38 +0200

    feat: entity ViewModels

---
+++ Vanilla-IRP/ViewModel/ListModelViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using MvvmFoundation.Wpf;
+using Vanilla_IRP.Domain;
+using Vanilla_IRP.Services;
+using Vanilla_IRP.View;
+using static System.Windows.Application;
+
+namespace Vanilla_IRP.ViewModel
+{
+   public class ListModelViewModel : ListDomainViewModel<Model>
+   {
+       public ListModelViewModel() : base() { }
+   }
+}
