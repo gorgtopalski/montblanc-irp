418430748e2f15f36585b38ded322fe72f7bf4c2
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-06 12:47:33 +0200

    feat: initial domain entities

---
+++ Vanilla-IRP/Domain/DefectType.cs
@@ -0,0 +1,54 @@
+﻿using LiteDB;
+using System.Collections.Generic;
+
+namespace Vanilla_IRP.Domain
+{
+    public class DefectType : IDomain
+    {
+        public int DefectTypeId { get; set; }
+        public int Severity { get; set; }
+        public string Name { get; set; }
+        
+        public BsonValue Id() => new BsonValue(DefectTypeId);
+
+        #region Equals, Hash, ToString
+        
+        public override string ToString() => Name;
+
+        protected bool Equals(DefectType other)
+        {
+            return Severity == other.Severity && string.Equals(Name, other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((DefectType) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Severity * 397) ^ (Name != null ? Name.GetHashCode() : 0);
+            }
+        }
+
+        private sealed class SeverityRelationalComparer : IComparer<DefectType>
+        {
+            public int Compare(DefectType x, DefectType y)
+            {
+                if (ReferenceEquals(x, y)) return 0;
+                if (ReferenceEquals(null, y)) return 1;
+                if (ReferenceEquals(null, x)) return -1;
+                return x.Severity.CompareTo(y.Severity);
+            }
+        }
+
+        public static IComparer<DefectType> SeverityComparer { get; } = new SeverityRelationalComparer();
+
+        #endregion
+    }
+}
