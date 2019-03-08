77b2704dfea4e0e465a85ed920151bc8b7388c35
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-08 10:40:10 +0200

    feat: add db separation

---
+++ Verallia Montblanc IRP/Database/ProductionDb.cs
@@ -0,0 +1,32 @@
+﻿using IRP.Domain;
+using LiteDB;
+
+namespace IRP.Database
+{
+    public class ProductionDb
+    {
+        private static ProductionDb _instance =  new ProductionDb();
+        private readonly LiteRepository _repository;
+
+        private ProductionDb()
+        {
+            //TODO verify folder creation
+            System.IO.Directory.CreateDirectory(Properties.Db.Default.ProductionDbPath);
+            _repository = new LiteRepository(GenerateConnectionString());
+
+            //Map relations
+            
+            //Index generation
+            var db = _repository.Database;
+        }
+
+        public static ProductionDb Instance() => (_instance ?? (_instance = new ProductionDb()));
+
+        public static LiteRepository Db() => (_instance ?? (_instance = new ProductionDb()))._repository;
+
+        public static string GenerateConnectionString()
+        {
+            return Properties.Db.Default.ProductionDbPath + "." + Properties.Db.Default.DbSuffix;
+        }
+    }
+}
