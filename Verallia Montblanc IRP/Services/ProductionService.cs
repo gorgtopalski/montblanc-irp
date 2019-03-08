56d806d876d72a5b37aabce6ef19ac47125256c6
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-08 10:41:16 +0200

    feat: move production to separate db feat: query active productions

--- Verallia Montblanc IRP/Services/ProductionService.cs
+++ Verallia Montblanc IRP/Services/ProductionService.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IRP.Database;
 using IRP.Domain;
 
 namespace IRP.Services
 {
     public class ProductionService : BaseService<Production>
     {
-        public ProductionService() : base() { }
+        public ProductionService() : base(ProductionDb.Db()) { }
 
-        public override List<Production> GetAll() => Db.Query<Production>().Include(x => x.Model).ToList();
+        public override List<Production> GetAll() => Db.Query<Production>()
+            .Include(x => x.Model)
+            .Include(x => x.Line)
+            .ToList();
+
+        public List<Production> GetActiveProductions => Db.Query<Production>()
+            .Include(x => x.Model)
+            .Include(x => x.Line)
+            .Where(x => x.End == null)
+            .ToList();
     }
 }
