342930366fbcaf8dff27770a16607aebefc5c816
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-13 13:27:56 +0200

    feat: WIP for palet and palet map view

---
+++ Verallia Montblanc IRP/ViewModel/PaletMapViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using IRP.Domain;
+using IRP.Services;
+
+namespace IRP.ViewModel
+{
+    public class PaletMapViewModel
+    {
+        private ObservableCollection<IPaletVm> _palets;
+
+        public ObservableCollection<IPaletVm> Palets => _palets;
+        
+        public PaletMapViewModel() { }
+
+        public PaletMapViewModel(Production production)
+        {
+            var service = new PaletService();
+
+            _palets = new ObservableCollection<IPaletVm>();
+            service.GetAllForProduction(production).ForEach( x => _palets.Add(new PaletViewModel(x)));
+        }
+    }
+}
---
+++ Verallia Montblanc IRP/ViewModel/PaletViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using IRP.Domain;
+using IRP.Utils;
+
+namespace IRP.ViewModel
+{
+    public class PaletViewModel : IPaletVm
+    {
+        public Palet Palet { get; }
+        public Brush BackgroundColor { get; }
+        public Brush BorderColor { get; }
+
+        public PaletViewModel() { }
+
+        public PaletViewModel(Palet palet)
+        {
+            Palet = palet;
+            BackgroundColor = PaletColor.GetBackgroundColor(palet);
+            BorderColor = PaletColor.GetBorderColor(palet);
+        }
+    }
+}
