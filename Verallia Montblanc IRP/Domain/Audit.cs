c5ded3e82e7f9ff075bc2c2d3273463b8401e1b8
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-13 11:11:00 +0200

    feat: added motives and audits for IRP generation

---
+++ Verallia Montblanc IRP/Domain/Audit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRP.Domain
+{
+    public class Audit
+    {
+        public int Number { get; set; }
+        public string Comment { get; set; }
+        public bool IsOk { get; set; }
+    }
+}
---
+++ Verallia Montblanc IRP/Domain/Motive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRP.Domain
+{
+    public class Motive
+    {
+        public Defect Defect { get; set; }
+        public int Number { get; set; }
+        public string Comment { get; set; }
+    }
+}
