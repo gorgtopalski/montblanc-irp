d0cbf414bdf63c44b97d3e96d0a10834e799f737
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-13 11:09:47 +0200

    feat: add team and shift enums

---
+++ Verallia Montblanc IRP/Domain/Shift.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRP.Domain
+{
+    public enum Shift
+    {
+        Night,
+        Morning,
+        Afternoon
+    } 
+}
---
+++ Verallia Montblanc IRP/Domain/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRP.Domain
+{
+    public enum Team
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five
+    }
+}
