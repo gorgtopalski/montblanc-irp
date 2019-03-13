5df397a31513b0454f386c3eda2a8cc0a612303d
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-13 13:29:44 +0200

    feat: interface for production vm

---
+++ Verallia Montblanc IRP/ViewModel/IProductionVm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using IRP.Domain;
+using MvvmFoundation.Wpf;
+
+namespace IRP.ViewModel
+{
+    public interface IProductionVm
+    {
+        Production Production { get; set; }
+
+        PaletCount Count { get; }
+        
+        int LastPalet { get; set; }
+
+        Brush BorderColor { get; }
+
+        Brush BackgroundColor { get; }
+
+        RelayCommand UpdateLastPalet { get; }
+    }
+}
