c2060ec2747cde3e962120e7d2d0f1e2262c01fd
Gueorgui Topalski <gueorgui@topalski.es>
2019-03-13 13:23:59 +0200

    feat: custom views for composing the irp view

---
+++ Verallia Montblanc IRP/View/Controls/ActiveProduction.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IRP.View.Controls
+{
+    /// <summary>
+    /// Lógica de interacción para ActiveProduction.xaml
+    /// </summary>
+    public partial class ActiveProduction : UserControl
+    {
+        public ActiveProduction()
+        {
+            InitializeComponent();
+        }
+    }
+}
